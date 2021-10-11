4d5fa7f9b988e1713e5273dcc6731255f50be378
Smolldo <maximqwerbich@gmail.com>
2021-10-11 10:47:45 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -3,8 +3,8 @@ const answ =document.querySelector('.answ');
 const split = (str) =>{
     let arr = [];
     arr = text.split(" ");
-    for(const char of str)
-      arr.push(char)
+    for(const index of str)
+      arr.push(index)
      
     return arr;
   }
